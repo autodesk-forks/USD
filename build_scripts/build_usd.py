14b15e156323047a0384ebabc4fbf88d07583450
Jerry Gamache <jerry.gamache@autodesk.com>
2021-12-15 15:12:27 -0500

    Restore original build_usd.py MaterialX options.

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1438,9 +1438,7 @@ MATERIALX_URL = "https://github.com/materialx/MaterialX/archive/v1.38.3.zip"
 
 def InstallMaterialX(context, force, buildArgs):
     with CurrentWorkingDirectory(DownloadURL(MATERIALX_URL, context, force)):
-        cmakeOptions = ['-DMATERIALX_BUILD_SHARED_LIBS=ON',
-                        '-DMATERIALX_NAMESPACE_SUFFIX=MayaUSD',
-                        '-DMATERIALX_LIBNAME_SUFFIX=MayaUSD']
+        cmakeOptions = ['-DMATERIALX_BUILD_SHARED_LIBS=ON']
 
         cmakeOptions += buildArgs;
 
