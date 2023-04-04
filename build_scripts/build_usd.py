79f860401428f7bcdb95159bf31a452d9a5f3286
Sean Donnelly <23455376+seando-adsk@users.noreply.github.com>
2023-04-04 15:04:31 -0400

    Python bindings for MaterialX

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1662,8 +1662,19 @@ MATERIALX_URL = "https://github.com/AcademySoftwareFoundation/MaterialX/archive/
 
 def InstallMaterialX(context, force, buildArgs):
     with CurrentWorkingDirectory(DownloadURL(MATERIALX_URL, context, force)):
+
+        pythonInfo = GetPythonInfo(context)
+
+        # Heavy patching required because mayapy does not produce valid build info
+        # when programmatically queried.
         cmakeOptions = ['-DMATERIALX_BUILD_SHARED_LIBS=ON',
-                        '-DMATERIALX_BUILD_TESTS=OFF'
+                        '-DMATERIALX_BUILD_TESTS=OFF',
+                        '-DMATERIALX_BUILD_PYTHON=ON',
+                        '-DMATERIALX_INSTALL_PYTHON=OFF',
+                        '-DMATERIALX_PYTHON_EXECUTABLE="{0}"'.format(pythonInfo[0]),
+                        '-DPYTHON_EXECUTABLE="{0}"'.format(pythonInfo[0]),
+                        '-DPYTHON_LIBRARY="{0}"'.format(pythonInfo[1]),
+                        '-DPYTHON_INCLUDE_DIR="{0}"'.format(pythonInfo[2])
         ]
 
         if MacOSTargetEmbedded(context):
@@ -1686,6 +1697,23 @@ def InstallMaterialX(context, force, buildArgs):
                        ], multiLineMatches=True)
 
         cmakeOptions += buildArgs
+
+        PatchFile(os.path.join('source', 'PyMaterialX', 'External', 'PyBind11', 'tools', 'FindPythonLibsNew.cmake'),
+                [('list(GET _PYTHON_VALUES 2 PYTHON_INCLUDE_DIR)',
+                'if (NOT DEFINED PYTHON_INCLUDE_DIR)\n' +
+                'list(GET _PYTHON_VALUES 2 PYTHON_INCLUDE_DIR)\n' +
+                'endif()'),
+                ('if(CMAKE_HOST_WIN32)',
+                'if(NOT DEFINED PYTHON_LIBRARY)\n' +
+                'if(CMAKE_HOST_WIN32)'),
+                ('mark_as_advanced(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)',
+                'endif()\n' +
+                'mark_as_advanced(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)')])
+
+        PatchFile(os.path.join('source', 'PyMaterialX', 'CMakeLists.txt'),
+                [('    set(MATERIALX_PYTHON_DEBUG_POSTFIX "_d")',
+                '    set(MATERIALX_PYTHON_DEBUG_POSTFIX "")')])
+
         RunCMake(context, force, cmakeOptions)
 
 MATERIALX = Dependency("MaterialX", InstallMaterialX, "include/MaterialXCore/Library.h")
