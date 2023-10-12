df0badca43889253b91ffc15f04abf089e6aa2ae
Jerry Gamache <jerry.gamache@autodesk.com>
2023-10-12 14:02:38 -0400

    EMSUSD-612 - Upgrade MaterialX to 1.38.8 (#586)
    Surprisingly little to do. An updated PyBind11 has better support to
    pre-define Python locations and does not require patching the
    FindPythonLibs.cmake anymore.

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1674,7 +1674,8 @@ def InstallMaterialX(context, force, buildArgs):
                         '-DMATERIALX_PYTHON_EXECUTABLE="{0}"'.format(pythonInfo[0]),
                         '-DPYTHON_EXECUTABLE="{0}"'.format(pythonInfo[0]),
                         '-DPYTHON_LIBRARY="{0}"'.format(pythonInfo[1]),
-                        '-DPYTHON_INCLUDE_DIR="{0}"'.format(pythonInfo[2])
+                        '-DPYTHON_INCLUDE_DIR="{0}"'.format(pythonInfo[2]),
+                        '-DPYBIND11_PYTHONLIBS_OVERWRITE=OFF'
         ]
 
         if MacOSTargetEmbedded(context):
@@ -1697,18 +1698,6 @@ def InstallMaterialX(context, force, buildArgs):
                        ], multiLineMatches=True)
 
         cmakeOptions += buildArgs
-
-        PatchFile(os.path.join('source', 'PyMaterialX', 'External', 'PyBind11', 'tools', 'FindPythonLibsNew.cmake'),
-                [('list(GET _PYTHON_VALUES 2 PYTHON_INCLUDE_DIR)',
-                'if (NOT DEFINED PYTHON_INCLUDE_DIR)\n' +
-                'list(GET _PYTHON_VALUES 2 PYTHON_INCLUDE_DIR)\n' +
-                'endif()'),
-                ('if(CMAKE_HOST_WIN32)',
-                'if(NOT DEFINED PYTHON_LIBRARY)\n' +
-                'if(CMAKE_HOST_WIN32)'),
-                ('mark_as_advanced(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)',
-                'endif()\n' +
-                'mark_as_advanced(PYTHON_LIBRARY PYTHON_INCLUDE_DIR)')])
 
         PatchFile(os.path.join('source', 'PyMaterialX', 'CMakeLists.txt'),
                 [('    set(MATERIALX_PYTHON_DEBUG_POSTFIX "_d")',
