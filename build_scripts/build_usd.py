ea39ae5caca9e660afb47b69c0f89ecca6891851
Eric Brosseau <eric.brosseau@autodesk.com>
2024-11-20 07:56:55 -0500

    Updating OpenEXR version to 3.3.1 (#1474)

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1180,7 +1180,7 @@ PNG = Dependency("PNG", InstallPNG, "include/png.h")
 ############################################################
 # IlmBase/OpenEXR
 
-OPENEXR_URL = "https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v3.1.11.zip"
+OPENEXR_URL = "https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v3.3.1.zip"
 
 def InstallOpenEXR(context, force, buildArgs):
     with CurrentWorkingDirectory(DownloadURL(OPENEXR_URL, context, force)):
