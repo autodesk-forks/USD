d97d336a7f7f7a96519def7b74367d5a6bc90ebf
Scot Brew <scot.brew@gmail.com>
2025-06-01 18:19:11 -0500

    Cleanup

--- extras/usd/web/window_state.h
+++ extras/usd/web/window_state.h
@@ -34,7 +34,7 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 static void error_callback(int error, const char *description)
 {
-    //TF_RUNTIME_ERROR(description); // issues a warning at compile time
+    TF_RUNTIME_ERROR("window_state error_callback: %s",description);
 }
 
 // GLFW window state data
